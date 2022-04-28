d7f4d1a0f1b2678e055f814e99f39011c881ebcc
ajaykumar-gbi <90805529+ajaykumar-gbi@users.noreply.github.com>
2022-04-28 15:03:23 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,17 @@
 #Get date
 now=$(date -I)
 ref=$(echo $GITHUB_REF | grep -o -E "\d+\.\d+\.*\d*")
-
+echp $ref
 # Get Github release data via GET
 releaseBody=$(curl --location --request GET "https://api.github.com/repos/$GITHUB_REPOSITORY/releases" \
 --header "Authorization: token $GITHUB_TOKEN" | jq ".[] | select(.tag_name==\"$ref\") | .name")
+echo $releaseBody
 
 # Get project id
 projectId=$(curl -l -X GET "$JIRA_BASE_URL/rest/api/2/project/" \
   -H "Authorization: Basic $JIRA_TOKEN" \
   | jq ".[] | select(.key==\"$JIRA_PROJECT_KEY\") | .id")
-
+echo $projectId
 # Create version
 
 echo "Version payload:"
