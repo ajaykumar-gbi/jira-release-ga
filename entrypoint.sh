34d6c7bed5a5ae8b79122820ac61aed0d214c8c6
ajaykumar-gbi <90805529+ajaykumar-gbi@users.noreply.github.com>
2022-04-30 12:58:48 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,13 @@
 #Get date
 now=$(date -I)
 echo $GITHUB_REF
+echo $TAG
 ref=$(echo $GITHUB_REF  | awk -F / '{print $(NF)}')
 echo $ref
-# Get Github release data via GET
-echo $GITHUB_REPOSITORY_OWNER 
-echo "github owner name"
-echo $GITHUB_REPOSITORY
-echo "repo name"
-curl --location --request GET "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" --header "Authorization: token $GITHUB_TOKEN" | jq ".body"
-releaseBody=$(curl --location --request GET "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" --header "Authorization: token $GITHUB_TOKEN" | jq ".body")
+
+curl --location --request GET "https://api.github.com/repos/$GITHUB_REPOSITORY/releases" --header "Authorization: token $GITHUB_TOKEN" | jq ".[] | select(.tag_name==\"$TAG\") | .name")
+releaseBody=$(curl --location --request GET "https://api.github.com/repos/$GITHUB_REPOSITORY/releases" --header "Authorization: token $GITHUB_TOKEN" | jq ".[] | select(.tag_name==\"$TAG\") | .name")
+echo "release body:"
 echo $releaseBody
 
 # Get project id
@@ -27,4 +25,4 @@ echo "{\"description\":$releaseBody,\"name\":\"$ref\",\"archived\":false,\"relea
 curl -l -X POST "$JIRA_BASE_URL/rest/api/2/version" \
   -H "Content-Type: application/json" \
   -H "Authorization: Basic $JIRA_TOKEN" \
-  --data-raw "{\"description\":\"$releaseBody\",\"name\":\"$ref\",\"archived\":false,\"released\":true,\"releaseDate\":\"$now\",\"projectId\":\"$projectId\"}"
+  --data-raw "{\"description\":\"$releaseBody\",\"name\":\"$TAG\",\"archived\":false,\"released\":true,\"releaseDate\":\"$now\",\"projectId\":\"$projectId\"}"
